ef5022bc7c00c676aa91a0a52f0988fd335acb50
rakesh 09 <rakeshmr9999@gmail.com>
2024-01-28 17:59:08 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='its a waffer project',
+    author='rakeshmr09',
+    license='MIT',
+)
